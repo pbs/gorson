fe2c4f61da0f36d3b85a1879d4f4db0c81fb0699
Chris MacDonald <cmacdonald@pbs.org>
2018-11-28 13:31:31 -0500

    fix linter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -uo pipefail
 IFS=$'\n\t'
 
+# I'd love a simpler way to do this check, but no native way seems to exist
+# https://github.com/golang/go/issues/24230
+# https://github.com/golang/go/issues/24427
+
+
 >&2 echo 'linting'
-invalid=$(gofmt -l . | grep -v vendor 2>&1)
-echo "a"
-if [ "$invalid" ]; then
-  echo "b"
+invalid="$(gofmt -s -l . | grep -v vendor)"
+test -z "$invalid"
+if [[ $? -ne 0 ]]; then
   >&2 echo "linting failed on the following files; ./scripts/format.sh should fix this"
   >&2 echo $invalid
   exit 1
 fi
+echo 'linting succeeded :)'
