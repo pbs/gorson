a07b4edf9f1027e227be7610dd647fb435ca63d7
Chris MacDonald <cmacdonald@pbs.org>
2018-11-28 12:22:17 -0500

    sort bash output

--- internal/gorson/bash/bash.go
+++ internal/gorson/bash/bash.go
@@ -1,13 +1,30 @@
 package bash
 
-import "strings"
+import (
+	"sort"
+	"strings"
+)
+
+func getKeys(parameters map[string]string) []string {
+	i := 0
+	keys := make([]string, len(parameters))
+	for k := range parameters {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
 
 // ParamsToShell generates a shell script to export environment variables
 func ParamsToShell(parameters map[string]string) string {
 	lines := make([]string, 0)
-	for key, value := range parameters {
+	keys := getKeys(parameters)
+	sort.Strings(keys)
+	for _, key := range keys {
+		// TODO sort
 		// TODO do we need to do extra escaping here?
-		lines = append(lines, "export "+key+"="+value)
+		v := parameters[key]
+		lines = append(lines, "export "+key+"="+v)
 	}
 	return strings.Join(lines, "\n")
 }
