7af4922672c0f4f3efc4adca28c67bff6b34b1cd
Josh Finnie <josh@jfin.us>
2019-11-26 14:58:27 -0500

    adding yaml support

--- cmd/get.go
+++ cmd/get.go
@@ -6,12 +6,22 @@ import (
 	"github.com/pbs/gorson/internal/gorson/io"
 	"github.com/pbs/gorson/internal/gorson/json"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
-func get(path string) {
+func get(path string, y bool) {
 	pms := io.ReadFromParameterStore(path)
-	marshalled := json.Marshal(pms)
-	fmt.Println(marshalled)
+	if y {
+		serialized, err := yaml.Marshal(pms)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(serialized))
+
+	} else {
+		marshalled := json.Marshal(pms)
+		fmt.Println(marshalled)
+	}
 }
 
 func init() {
@@ -20,9 +30,11 @@ func init() {
 		Short: "Get parameters from a parameter store path",
 		Run: func(cmd *cobra.Command, args []string) {
 			path := args[0]
-			get(path)
+			y, _ := cmd.Flags().GetBool("yaml")
+			get(path, y)
 		},
 		Args: cobra.ExactArgs(1),
 	}
+	cmd.Flags().Bool("yaml", false, "get command outputs as yaml")
 	rootCmd.AddCommand(cmd)
 }
