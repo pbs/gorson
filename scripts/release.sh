1ca01c5f8a5f43eb6a0923bd4550a9bab3914f9b
Chris MacDonald <cmacdonald@pbs.org>
2020-09-22 15:55:05 -0700

    wip

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,14 @@ IFS=$'\n\t'
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 CURRENT=$(cat ./internal/gorson/version/version.go | grep 'const Version' | cut -d'"' -f2)
-echo $CURRENT
 NEW=`expr $CURRENT + 1`
 
-sed -i -e "s/-$CURRENT-/-$NEW-/g" README.md
+sed "-i" "" "-e" "s/-$CURRENT-/-$NEW-/g" README.md
+sed "-i" "" "-e" "s/$CURRENT/$NEW/g" ./internal/gorson/version/version.go
+
+git checkout -b "release/$NEW"
+git add README.md ./internal/gorson/version/version.go
+git commit -m "release $NEW"
+git tag $NEW
+git push
+git push --tags
