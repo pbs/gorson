5ed9efac0f63abe168787d298b5eae20083d60eb
Casey Boettcher <chb@socketbox.com>
2022-11-17 16:15:45 -0500

    Refactoring to reduce duplication across bash and env

--- internal/gorson/bash/bash.go
+++ internal/gorson/bash/bash.go
@@ -2,21 +2,17 @@ package bash
 
 import (
 	"fmt"
-	"sort"
-	"strings"
-
 	"github.com/pbs/gorson/internal/gorson/util"
+	"strings"
 )
 
 // ParamsToShell generates a shell script to export environment variables
 func ParamsToShell(parameters map[string]string) string {
-	lines := make([]string, 0)
-	keys := util.GetKeys(parameters)
-	sort.Strings(keys)
-	for _, key := range keys {
-		v := parameters[key]
-		line := fmt.Sprintf("export %s='%s'", key, v)
-		lines = append(lines, line)
+	lines := util.ParmsToArray(parameters)
+	expLines := make([]string, len(lines))
+	for i, line := range lines {
+		expLine := fmt.Sprintf("export %s", line)
+		expLines[i] = expLine
 	}
-	return strings.Join(lines, "\n")
+	return strings.Join(expLines, "\n")
 }
--- internal/gorson/env/env.go
+++ internal/gorson/env/env.go
@@ -1,8 +1,6 @@
 package env
 
 import (
-	"fmt"
-	"sort"
 	"strings"
 
 	"github.com/pbs/gorson/internal/gorson/util"
@@ -10,13 +8,6 @@ import (
 
 // Marshal env-formats parameters
 func Marshal(parameters map[string]string) string {
-	lines := make([]string, 0)
-	keys := util.GetKeys(parameters)
-	sort.Strings(keys)
-	for _, key := range keys {
-		v := parameters[key]
-		line := fmt.Sprintf("%s='%s'", key, v)
-		lines = append(lines, line)
-	}
+	lines := util.ParmsToArray(parameters)
 	return strings.Join(lines, "\n")
 }
