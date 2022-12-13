b16ee670da139afed3d28aa75fbcaec99c9516a1
Casey Boettcher <chb@socketbox.com>
2022-12-13 10:01:08 -0500

    Check for bad keys and quoted values

--- internal/gorson/util/util.go
+++ internal/gorson/util/util.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 	"golang.org/x/exp/maps"
+	"regexp"
 	"sort"
 	"strings"
 )
@@ -27,14 +28,27 @@ func NewParameterStorePath(input string) *ParameterStorePath {
 	return &ParameterStorePath{filtered}
 }
 
-func ParmsToArray(parameters map[string]string) []string {
+// ParametersToSlice accepts a map of string key/value pairs and returns an array of strings.
+// The elements of the returned array are keys and values conjoined by an `=` sign.
+// Keys that do not start with a letter or underscore will result in an error.
+// All values are enclosed in single quotes.
+// Values that contain single quote characters will first be converted to double quotes.
+func ParametersToSlice(parameters map[string]string) ([]string, error) {
 	lines := make([]string, 0)
 	keys := maps.Keys(parameters)
 	sort.Strings(keys)
+	//envvar names must begin with a letter or underscore
+	invalidKey := regexp.MustCompile(`^[^a-zA-Z_]`)
 	for _, key := range keys {
+		if invalidKey.MatchString(key) {
+			return nil, fmt.Errorf("Key %s invalid", key)
+		}
 		v := parameters[key]
+		if strings.Contains(v, "'") {
+			v = strings.ReplaceAll(v, "'", "\"")
+		}
 		line := fmt.Sprintf("%s='%s'", key, v)
 		lines = append(lines, line)
 	}
-	return lines
+	return lines, nil
 }
